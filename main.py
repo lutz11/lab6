39e320d6ad816bab1aefde9eb75b61994fb13bc2
lutz11 <163760197+lutz11@users.noreply.github.com>
2024-03-19 10:15:50 -0400

    Update main.py to include comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Name: Luke Kutz-Pears
 # Date: 03/18/2024
 
+# encode password function which increments numerical password by three numbers
 def encode(password):
     encoded_pass = ''
     for i in password:
@@ -13,15 +14,18 @@ def main():
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         choice = int(input('Please enter an option: '))
 
+        # option 1 encodes the provided password
         if choice == 1:
             password = input('Please enter your password to encode: ')
             encoded_password = encode(password)
             print('Your password has been encoded and stored!\n')
 
+        # option 2 decodes the encoded password, which returns the original password
         if choice == 2:
             # TODO: Implement decoder function and change None placeholder -> original password
             print(f'The encoded password is {encoded_password}, and the original password is {None}')
 
+        # option 3 exits the program
         if choice == 3:
             break
 
